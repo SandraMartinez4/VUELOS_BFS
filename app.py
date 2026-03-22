79e7c142fea7c5d066d0a23face054a1edf52d23
SandraMartinez4 <sandraallendemartinez1456@gmail.com>
2026-03-22 16:55:36 -0600

    vuelos BFS10

--- app.py
+++ app.py
@@ -28,5 +28,10 @@ def index():
 
     return render_template("index.html", ciudades=ciudades, ruta=ruta)
 
+
+
+
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
